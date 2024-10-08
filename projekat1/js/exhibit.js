2008712304ef57907e53bfb7ddbb19ea60d84caf
Danilo <daniloradovic2003@gmail.com>
2024-10-08 19:26:14 +0200

    exhibit

--- projekat1/js/exhibit.js
+++ projekat1/js/exhibit.js
@@ -1,5 +1,6 @@
 function bodyLoaded() {
     loadDepartments();
+    loadExhibit();
 }
 
 function loadDepartments() {
@@ -17,5 +18,25 @@ function loadDepartments() {
 
                 document.getElementById('depts_nav').innerHTML += dept_nav_html;
             });
+        });
+}
+
+function loadExhibit() {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const id = urlParams.get('id');
+
+    fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${id}`)
+        .then(response => response.json())
+        .then(artwork => {
+            document.getElementById("image").src = artwork.primaryImageSmall || `img/placeholder.png`;
+            const listAttr = document.getElementById("attr_list");
+            listAttr.innerHTML = "";
+            listAttr.innerHTML += `<li class="list - group - item text-light">title: ${artwork.title}</li>`;
+            listAttr.innerHTML += `<li class="list - group - item text-light">Name: ${artwork.objectName}</li>`;
+            listAttr.innerHTML += `<li class="list - group - item text-light">ID: ${artwork.objectID}</li>`;
+            listAttr.innerHTML += `<li class="list - group - item text-light">department: ${artwork.department}</li>`;
+            listAttr.innerHTML += `<li class="list - group - item text-light">Highlighted: ${artwork.isHighlight}</li>`;
+            listAttr.innerHTML += `<li class="list - group - item text-light">public domain: ${artwork.isPublicDomain}</li>`;
         });
 }
