9f529557be55464aa2d4cd683d02f3faff24b7fa
Danilo <daniloradovic2003@gmail.com>
2024-10-30 18:15:33 +0100

    routing

--- projekat2/SJ-ColabDraw/src/router/index.ts
+++ projekat2/SJ-ColabDraw/src/router/index.ts
@@ -1,19 +1,45 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import { getAllJSDocTags } from 'typescript';
+
 import Home from '@/views/Home.vue';
-import About from '@/views/About.vue';
+import Login from '../views/Login.vue';
+import Register from '../views/Register.vue';
+import Gallery from '../views/Gallery.vue';
+import Image from '../views/Image.vue';
+import Drawing from '../views/Drawing.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
+      name: 'Home',
       component: Home,
     },
     {
-      path: '/about/',
-      name: 'about',
-      component: About,
+      path: '/register',
+      name: 'Register',
+      component: Register,
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login,
+    },
+    {
+      path: '/gallery',
+      name: 'Gallery',
+      component: Gallery,
+    },
+    {
+      path: '/image/:id',
+      name: 'Image',
+      component: Image,
+    },
+    {
+      path: '/draw/:id',
+      name: 'Drawing',
+      component: Drawing,
     },
   ],
 });
