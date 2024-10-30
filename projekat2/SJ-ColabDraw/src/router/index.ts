2e9e4c25eefcc0161ccb4caa73b0d4763e960d25
Danilo <daniloradovic2003@gmail.com>
2024-10-30 19:15:30 +0100

    Revert "Added Vuetify, Eslint and Prettier and sample code"
    This reverts commit 5a1e7141fadf775b085e2b762a68b17557b6a323.

--- projekat2/SJ-ColabDraw/src/router/index.ts
+++ projekat2/SJ-ColabDraw/src/router/index.ts
@@ -1,7 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '@/views/Home.vue'
-import About from '@/views/About.vue'
-
+import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,12 +7,15 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: HomeView
     },
     {
-      path: '/about/',
+      path: '/about',
       name: 'about',
-      component: About
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
     }
   ]
 })
