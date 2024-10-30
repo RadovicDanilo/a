99e4d16fb530aa96803b0c151da930ba58846adc
Danilo <daniloradovic2003@gmail.com>
2024-10-30 20:03:26 +0100

    navbar

--- projekat2/ColabDraw/src/App.vue
+++ projekat2/ColabDraw/src/App.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
-import NavBar from "./components/NavBar.vue";
+import { RouterView } from "vue-router";
+import Navbar from "@/components/Navbar.vue";
 </script>
 
 <template>
-  <NavBar />
+  <Navbar />
   <RouterView />
 </template>
 
--- projekat2/ColabDraw/src/router/index.ts
+++ projekat2/ColabDraw/src/router/index.ts
@@ -1,45 +1,45 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/HomeView.vue';
-import Login from '../views/LoginView.vue';
-import Register from '../views/RegisterView.vue';
-import Gallery from '../views/GalleryView.vue';
-import Image from '../views/ImageView.vue';
-import Drawing from '../views/DrawingView.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/HomeView.vue";
+import Login from "../views/LoginView.vue";
+import Register from "../views/RegisterView.vue";
+import Gallery from "../views/GalleryView.vue";
+import Image from "../views/ImageView.vue";
+import Drawing from "../views/DrawingView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-   {
-      path: '/',
-      name: 'home',
+    {
+      path: "/",
+      name: "home",
       component: Home,
     },
     {
-      path: '/register',
-      name: 'register',
+      path: "/register",
+      name: "register",
       component: Register,
     },
     {
-      path: '/login',
-      name: 'login',
+      path: "/login",
+      name: "login",
       component: Login,
     },
     {
-      path: '/gallery',
-      name: 'gallery',
+      path: "/gallery",
+      name: "gallery",
       component: Gallery,
     },
     {
-      path: '/image/:id',
-      name: 'image',
+      path: "/image/:id",
+      name: "image",
       component: Image,
     },
     {
-      path: '/draw/:id',
-      name: 'drawing',
+      path: "/draw/:id",
+      name: "drawing",
       component: Drawing,
-    }
-  ]
-})
+    },
+  ],
+});
 
-export default router
+export default router;
