193fd1c0a35f88dfbed7bfa1d4c8caf7569aa100
Danilo <daniloradovic2003@gmail.com>
2024-12-02 17:22:03 +0100

    Added Vuetify

--- ColabDraw/src/main.ts
+++ ColabDraw/src/main.ts
@@ -3,13 +3,26 @@ import { createPinia } from "pinia";
 
 import App from "./App.vue";
 import router from "./router";
-import "@mdi/font/css/materialdesignicons.css";
+
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
+
 import DarkReader from "darkreader";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
 
 const app = createApp(App);
 
+app.use(vuetify);
 app.use(createPinia());
 app.use(router);
+
 
 app.mount("#app");
 
