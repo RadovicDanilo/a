9b8f8b2d68f3bad1d680d4fd523d7d357a67871d
Danilo <daniloradovic2003@gmail.com>
2024-12-04 18:34:42 +0100

    small fix

--- ColabDraw/src/views/ViewLogin.vue
+++ ColabDraw/src/views/ViewLogin.vue
@@ -43,7 +43,7 @@ export default defineComponent({
         username: values.username,
         password: values.password
       }
-      const success = authStore.login(loginReq);
+      const success = await authStore.login(loginReq);
       if (!success) {
         return;
       }
